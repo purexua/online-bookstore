2031e103369a6b06bc387e57e25a3d730c55c871
purexua <xujh3919@gmail.com>
2023-09-28 19:46:43 +0800

    add aop part Aspect class

---
+++ database-design/src/main/java/com/purexua/config/MyAspect.java
@@ -0,0 +1,14 @@
+package com.purexua.config;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Aspect
+@Component
+public class MyAspect {
+
+}
