c4146df51dc72ba143bef2c76a2e80bf385f9323
purexua <xujh3919@gmail.com>
2023-11-08 19:40:21 +0800

    beautified the homepage interface

--- online-bookstore-management/src/views/index/Home.vue
+++ online-bookstore-management/src/views/index/Home.vue
@@ -1,11 +1,51 @@
 <template>
-  <h1>这是home组件</h1>
+  <div>
+    <el-carousel :interval="4000" type="card" height="300px">
+      <el-carousel-item v-for="item in 5" :key="item">
+        <img :src="require(`@/assets/images/${item}.jpg`)" class="image-style">
+      </el-carousel-item>
+    </el-carousel>
+
+    <div class="introduction">
+      <!-- <h1>Welcome to Our Online Bookstore</h1>
+      <p>Explore a World of Knowledge and Adventure</p>
+      <p>Find your next favorite book from our extensive collection.</p>
+      <p>Enjoy a seamless shopping experience and discover the joy of reading.</p> -->
+      <h1>Welcome to Our Online Bookstore</h1>
+      <p>探索知识和冒险的世界</p>
+      <p>从我们丰富的藏书中找到您下一本喜爱的书籍</p>
+      <p>享受无缝的购物体验，发现阅读的乐趣</p>
+    </div>
+
+  </div>
 </template>
-  
+
 <script>
 export default {
-  name: 'home',
-}
+  name: 'Home',
+};
 </script>
-  
-<style></style>
+
+<style scoped>
+.image-style {
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+}
+
+.introduction {
+  text-align: center;
+  margin-top: 30px;
+}
+
+.introduction h1 {
+  font-size: 2.5em;
+  margin-bottom: 20px;
+}
+
+.introduction p {
+  font-size: 1.2em;
+  margin-bottom: 10px;
+  color: #666;
+}
+</style>
